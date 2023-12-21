8899f68e9daffd632107070152f3d16b1b9fb80c
Ramazan <ismayiloff522@gmail.com>
2023-12-22 01:19:07 +0400

    commit

--- js/login.js
+++ js/login.js
@@ -1,3 +1,12 @@
+const preLoader = document.querySelector('.preloader');
+const myPage = document.querySelector('.mypage');
+
+setTimeout(() => {
+    preLoader.style.display = 'none';
+    myPage.style.display = 'block';
+}, 2000);
+
+
 const inputOne = document.querySelector('.inputOne');
 const inputTwo = document.querySelector('.inputTwo');
 
